ebbf56cf13f1c98987202070d97482a6aae17e47
rpytel1 <rafal.pytel1@wp.pl>
2018-01-03 14:40:55 +0100

    adding snort

--- sfc-demo/sfc-firewall-test-topo.py
+++ sfc-demo/sfc-firewall-test-topo.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     sw1 = sfc.addSw()                         #(2) Add the SFFs
     sw2 = sfc.addSw()
     sw3 = sfc.addSw()
+    sw4 = sfc.addSw()
 
     h1 = sfc.addHost(sw1)                     #(3) Add host to be the src/dst of the chain
     h2 = sfc.addHost(sw1)
@@ -20,21 +21,24 @@ if __name__ == "__main__":
     h4 = sfc.addHost(sw1)
 
     sf1 = sfc.addSf('1', sw2, 'fw')           #(4) Add SFs with the SFF which it is connected and the SF type
-    sf2 = sfc.addSf('2', sw2, 'dpi')
-    sf3 = sfc.addSf('3', sw3, 'fw1')
-    sf4 = sfc.addSf('4', sw3, 'nat')
+    sf2 = sfc.addSf('2', sw3, 'dpi')
+    sf3 = sfc.addSf('3', sw4, 'fw1')
+
 
     sfc.addLink(sw1, sw2)                     #(5) Add link among SFFs
     sfc.addLink(sw2, sw3)
     sfc.addLink(sw3, sw1)
+    sfc.addLink(sw1, sw4)
+    sfc.addLink(sw2, sw4)
+    sfc.addLink(sw3, sw4)
 
     sfc.addGw(sw1)                            #(6) Add gateway at the end of the chain
 
-    chain = ['fw', 'dpi']       #(7) The chain (list of SFs)
+    chain = ['fw', 'dpi', 'fw1']       #(7) The chain (list of SFs)
 
     sfc.addChain('c1', sw1, chain, True)
-    chain = ['fw1']                            #(8) Add the chain with name, classifier and the chain (list of SFs)
-    sfc.addChain('c2', sw1, chain, False)
+    # chain = ['fw1']                            #(8) Add the chain with name, classifier and the chain (list of SFs)
+    # sfc.addChain('c2', sw1, chain, False)
 
     sfc.deployTopo()                          #(9) Deploy topology and chain configuration
 
